c21624962b6c7e0f93e2e3dce2b8ccf88fb56a44
William Whitworth <william10charles@gmail.com>
2017-03-28 09:48:07 -0600

    built initial file structure

---
+++ index.js
@@ -0,0 +1,5 @@
+const ui = require("./lib/ui");
+const searching = require("./lib/searching");
+const saving = require("./lib/saving");
+const loading = require("./lib/loading");
+const dictionaryData = require("./lib/dictionaryData");
