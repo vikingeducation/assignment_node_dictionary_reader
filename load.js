b1fe5b3e225f10035fae65028ab5a940f6ea9bc0
markmarkyesyes <markhahn545@gmail.com>
2017-03-28 12:11:30 -0500

    updates load.js to last commit

--- load.js
+++ load.js
@@ -1,1 +1,13 @@
-load.js
+var fs = require('fs')
+
+var loadDict = {
+  "loader": function(filename){
+    var path = `./data/${filename}`;
+    fs.readFileSync(path, 'utf8', (err, data) => {
+      if (err) throw err;
+      return data 
+    });
+  };
+};
+
+module.exports = loadDict;
