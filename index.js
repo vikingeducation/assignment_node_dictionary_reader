1955077668df7b05e57a8f86b3f1d99f33a55f63
nicoasp <nicolas.amaya@gmail.com>
2017-03-28 18:09:31 +0200

    create modules task 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const ui = require('ui');
+const loader = require('loader');
+
+
+
+ui.startProgram
+const dictionaries = loader.findDictionaries();
+ui.chooseDictionary(dictionaries);
+dict.loadFile
