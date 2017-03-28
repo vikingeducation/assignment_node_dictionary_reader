0d9b768e212388f5a84acb57050c7bfae5d714a6
Jerry Gao <gao@blackwright.com>
2017-03-28 16:35:21 -0400

    finish dictionary word count, letter count

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-let CLIModule = require('./lib/CLIModule');
-let cli = new CLIModule;
-let loader = require('./lib/loader');
-let dictionary = require('./lib/dictionary');
+const cli = require('./lib/cliModule');
+const loader = require('./lib/loader');
+const Dictionary = require('./lib/dictionary');
+
+const path = './data';
 
 let fileList;
-let path = './data'
 
 cli.intro();
-loader.getJSON(path).then(function(f){
-  fileList  = f;
+
+loader.getJSON(path).then( (files) => {
+  fileList = files;
   cli.listFiles(fileList);
-})
+});
 
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
+
 process.stdin.on('data', (data) => {
   data = data.trim();
-  if (data === 'q') {
-    process.exit();
-  } else {
-    let choice = cli.chooseFile(data, fileList);
-    // TODO: we shouldn't have all of the logic inside this if (choice) block.. there's probably a better way
-    if (choice) {
-      dictionary.load(path, choice).then(function(f){
-        console.log(f.toString());
-      });
-    }
+  if (data === 'q') process.exit();
+
+  const fileName = cli.chooseFile(data, fileList);
+
+  if (fileName) {
+    const dic = new Dictionary(path, fileName);
+    cli.printWordCount(dic.wordCount());
+    cli.printWordFreq(dic.wordFreq);
   }
 });
-
-//
-
