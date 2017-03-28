8c776371cbc540e06d521751c2c9a24b83ea8cd0
tim5046 <tim5046@gmail.com>
2017-03-28 14:56:22 -0700

    add states wtf

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 const cli = require('./lib/cliModule');
-const loader = require('./lib/loader');
-const Dictionary = require('./lib/dictionary');
+// const loader = require('./lib/loader');
+// const Dictionary = require('./lib/dictionary');
 
-const path = './data';
-
-let fileList;
-
-cli.intro();
-
-loader.getJSON(path).then( (files) => {
-  fileList = files;
-  cli.listFiles(fileList);
-});
+// // const path = './data';
+// let fileList;
 
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+cli.render('welcome');
 
 
-process.stdin.on('data', (data) => {
-  data = data.trim();
-  if (data === 'q') process.exit();
 
-  const fileName = cli.chooseFile(data, fileList);
 
-  if (fileName) {
-    const dic = new Dictionary(path, fileName);
-    cli.printWordCount(dic.wordCount());
-    cli.printWordFreq(dic.wordFreq);
 
-    cli.searchPrompt();
-    const searchType = search.runSearchType(data);
 
-    if (searchType) {
 
-    }
-  }
-});
+// cli.intro();
+
+// loader.getJSON(path).then( (files) => {
+//   fileList = files;
+//   cli.listFiles(fileList);
+// });
+
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf8');
+
+
+// process.stdin.on('data', (data) => {
+//   data = data.trim();
+//   if (data === 'q') process.exit();
+
+//   const fileName = cli.chooseFile(data, fileList);
+
+//   if (fileName) {
+//     const dic = new Dictionary(path, fileName);
+//     cli.printWordCount(dic.wordCount());
+//     cli.printWordFreq(dic.wordFreq);
+
+//     cli.searchPrompt();
+//     const searchType = search.runSearchType(data);
+
+//     if (searchType) {
+
+//     }
+//   }
+// });
 
--- states/welcome.js
+++ states/welcome.js
@@ -1,10 +1,42 @@
-welcomeState = {
+const loader = require('../lib/loader');
 
-  prompt: () => {
-      console.log(`
+const welcomeState = {
+  header: `
 Welcome to the Node Dictionary Reader!
 ======================================
 Enter q to quit
-      `);
+  `,
+
+  prompt: () => {
+    console.log(welcomeState.header);
+    let fileList = welcomeState.getFiles()
+
+    return(`
+    Select a dictionary to load:
+    ${fileList}
+    `);
+  },
+
+  getFiles: () => {
+    let fileList;
+    loader.getJSON('./data').then( (files ) => {
+      fileList = files;
+      return fileList;
+    })
   }
 };
+
+module.exports = (data) => {
+  return welcomeState
+};
+
+//   listFiles: (fileArray) => {
+//     fileArray.forEach( (fileName, index) => {
+//       console.log(`${index + 1}. ${fileName}`);
+//     })
+//   },
+
+// loader.getJSON(path).then( (files) => {
+//   fileList = files;
+//   cli.listFiles(fileList);
+// });
