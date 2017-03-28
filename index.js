a08c68c3fdc5761e11488003aa0b8a655798a9c8
William Whitworth <william10charles@gmail.com>
2017-03-28 10:06:29 -0600

    broke it giving away

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const ui = require("./lib/ui");
 const searching = require("./lib/searching");
 const saving = require("./lib/saving");
-const loading = require("./lib/loading");
+
 const dictionaryData = require("./lib/dictionaryData");
+
+ui.runUI();
