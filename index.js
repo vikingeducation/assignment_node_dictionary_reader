196f4d5037b3146f8d63eafce423ec92e406f45c
deven1 <deven@sloan.mit.edu>
2017-03-28 18:31:17 -0400

    intermediate save

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ var displayDicts = function() {
 	});
 }
 
-
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 ui.welcome
 displayDicts();
 
-process.stdin.on('data', (str) => {
+var onDataMenu = function (str) {
 	str = str.trim();
 	//need q to quit conditional
 	var selection = parseInt(str)
@@ -28,11 +27,26 @@ process.stdin.on('data', (str) => {
 		console.error("please enter a number from the selected");
 	} else {
 		selection--;
-		var selectedDictionary = dictDir[str-1];
+		var selectedDictionary = dictDir[selection];
 		load.loader(selectedDictionary);
 		load.letterCount();
-	} //selected items display 
-		//search prompt
+		process.stdin.removeListener('data', onDataMenu);
+		// process.stdin.removeListener('data',(err) => {
+		// 	if (err) throw err;
+		// });
+	} //selected items display
+}
+
+process.stdin.on('data', onDataMenu);
+
+var onDataSearch = function (str) {
+	str = str.trim();
+	//need q to quit conditional
+	var selection = parseInt(str)
+	//Validation func
+	if (isNaN(selection) || selection > dictDir.length || selection < 1) {
+		console.error("please enter a number from the selected");
+	} else {
 		console.log(
 		`What kind of search?
 		1: Exact
@@ -40,7 +54,6 @@ process.stdin.on('data', (str) => {
 		3: Begins With
 		4: Ends With`
 		)
-});
-
-
-
+	} //selected items display
+}
+process.stdin.on('data', onDataSearch);
