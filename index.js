3eafbab9272d5002853eb2991cee0433b8cbee89
tim5046 <tim5046@gmail.com>
2017-03-28 10:56:08 -0700

    successfully load in the chosen file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let CLIModule = require('./lib/CLIModule');
 let cli = new CLIModule;
 let loader = require('./lib/loader');
+let dictionary = require('./lib/dictionary');
 
 let fileList;
+let path = './data'
 
 cli.intro();
-loader.getJSON('./data').then(function(f){
+loader.getJSON(path).then(function(f){
   fileList  = f;
   cli.listFiles(fileList);
 })
@@ -19,8 +21,13 @@ process.stdin.on('data', (data) => {
   if (data === 'q') {
     process.exit();
   } else {
-    cli.handleInput(data, cli.chooseFile, fileList);
-    console.log('got here');
+    let choice = cli.chooseFile(data, fileList);
+    // TODO: we shouldn't have all of the logic inside this if (choice) block.. there's probably a better way
+    if (choice) {
+      dictionary.load(path, choice).then(function(f){
+        console.log(f.toString());
+      });
+    }
   }
 });
 
