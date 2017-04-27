e12d3d476f9ddc15a2b09efb4a6cf30fe095b1f5
rabGIT <rabGIT@users.noreply.github.com>
2017-04-27 14:32:49 -0700

    warmup

---
+++ warmup/three.js
@@ -0,0 +1,49 @@
+function three() {
+
+  // Start listening to STDIN
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  // Inline function to handle
+  // message output
+  var showMessage = (err) => {
+    console.log('State Three');
+    console.log('Type "next" to continue');
+    if (err) {
+      console.error(err);
+    }
+  };
+
+  // Display message
+  showMessage();
+
+
+  // Handler for STDIN data
+  // event
+  var onData = (data) => {
+    data = data.trim();
+
+    // If user input "next"
+    // let's go to the next
+    // state
+    if (data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+
+      // ----------------------------------------
+      // Go to next view here
+      // ----------------------------------------
+      console.log('going away forever');
+
+    } else {
+
+      // All other input is invalid
+      showMessage(`Invalid: ${ data }`);
+    }
+  };
+
+  // Set the listener
+  process.stdin.on('data', onData);
+}
+
+module.exports = { three };
