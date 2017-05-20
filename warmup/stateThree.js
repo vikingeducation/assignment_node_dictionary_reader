041e90122156100d521457a1e8c1d98e93beb082
Malyar Baron <malbaron0@gmail.com>
2017-05-20 15:36:32 -0400

    Warmup - Simple approach to handling states

---
+++ warmup/stateThree.js
@@ -0,0 +1,47 @@
+let three = function () {
+
+  // Start listening to STDIN
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  // Inline function to handle
+  // message output
+  var showMessage = (err) => {
+    console.log('State three');
+    console.log('Type "next" to continue');
+    if (err) {
+      console.error(err);
+    }
+  };
+
+  // Display message
+  showMessage();
+
+
+  // Handler for STDIN data
+  // event
+  var onData = (data) => {
+    data = data.trim();
+
+    // If user input "next"
+    // let's go to the next
+    // state
+    if (data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+
+      console.log('Goodbye!')
+      process.exit();
+      //second state
+    } else {
+
+      // All other input is invalid
+      showMessage(`Invalid: ${ data }`);
+    }
+  };
+
+  // Set the listener
+  process.stdin.on('data', onData);
+}
+
+module.exports.three = three;
