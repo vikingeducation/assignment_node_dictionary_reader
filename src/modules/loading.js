c4631bd1fe971b302c5168e2a31c0a38a410beb3
Ian Halverson <halversoncode@gmail.com>
2017-07-18 13:33:08 -0400

    trying to get key letter counts

--- src/modules/loading.js
+++ src/modules/loading.js
@@ -14,11 +14,10 @@ var selectedDictionary;
 var FindDictionary = function (str){
   str = str.trim();
   if (str === "1") {
-    console.log("hello");
-
-   var selectedDictionary = JSON.stringify(dictionary);
-   console.log(selectedDictionary[0]);
-
+    console.log("Sucessfully loaded your dictionary!");
+   var selectedDictionary = dictionary;
+   console.log("Word count: " + Object.keys(selectedDictionary).length);
+   console.log(letterFrequency(selectedDictionary));
    return true
   }
   else {
@@ -36,4 +35,27 @@ var FindDictionary = function (str){
 // B: 456
 
 }
+
+var letterFrequency = function(dict) {
+  var letterFrequencyObj = {
+    "a": 0,
+    "b": 0,
+    "c": 0
+  }
+  for(i=0;i<Object.keys(dict).length;i++) {
+    var word = dict[i];
+    var firstLetter = word[0].toString();
+    if (firstLetter === undefined) {
+      letterFrequencyObj[dict[i]] = 1;
+    }
+    else {
+      letterFrequencyObj.firstLetter += 1;
+    }
+    console.log(word + firstLetter + letterFrequencyObj);
+  }
+
+  return letterFrequencyObj;
+}
+
+
 module.exports = { "FindDictionary": FindDictionary}
