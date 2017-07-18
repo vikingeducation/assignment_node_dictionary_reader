8530fd34c0fc48ad76aa31887eda7ad0b2abe0f8
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-18 15:39:02 +0000

    warmup

---
+++ warmup/index.js
@@ -0,0 +1,106 @@
+function one() {
+
+  // Start listening to STDIN
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  // Inline function to handle
+  // message output
+  var showMessage = (err) => {
+    console.log('State one');
+    console.log('Type "next" to continue');
+    if (err) {
+      console.error(err);
+    }
+  };
+
+  // Display message
+  showMessage();
+
+
+  // Handler for STDIN data
+  // event
+  var onData = (data) => {
+    data = data.trim();
+
+    // If user input "next"
+    // let's go to the next
+    // state
+    if (data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+
+      two();
+
+    } else {
+
+      // All other input is invalid
+      showMessage(`Invalid: ${ data }`);
+    }
+  };
+
+  // Set the listener
+  process.stdin.on('data', onData);
+}
+
+// Start the app
+one();
+
+
+function two() {
+  console.log('two');
+  // Start listening to STDIN
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  // Inline function to handle
+  // message output
+
+  // Display message
+
+  // Handler for STDIN data
+  // event
+  var onData = (data) => {
+    data = data.trim();
+
+    // If user input "next"
+    // let's go to the next
+    // state
+    if (data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+
+      three();
+
+    } else {
+
+      // All other input is invalid
+      showMessage(`Invalid: ${ data }`);
+    }
+  };
+
+  // Set the listener
+  process.stdin.on('data', onData);
+  }
+
+function three() {
+  console.log('three');
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  var onData = (data) => {
+    console.log('here')
+    data = data.trim();
+
+    if (data === 'next') {
+      console.log('Goodbye')
+      process.stdin.removeListener('data', onData)
+      process.stdin.exit();
+
+    } else {
+      showMessage(`Invalid: ${ data }`);
+    }
+
+  };
+
+}
