892fdfd7cf9579afa8f70a055ffc2775c5438299
Ian Halverson <halversoncode@gmail.com>
2017-07-18 15:44:34 -0400

    finished state one

--- src/modules/cli_interface.js
+++ src/modules/cli_interface.js
@@ -1,33 +1,27 @@
 var load = require('./loading');
 
-
-
-
 var StateOne = function(){
   process.stdin.resume();
   process.stdin.setEncoding("utf8");
+  load.displayDictionaries();
     var onData = (data) => {
-      //load.FindDictionary(data);
       if(load.FindDictionary(data) != false){
         process.stdin.removeListener('data', onData);
            return StateTwo();
        }
     }
-
-
   process.stdin.on('data', onData);
+}
+var StateTwo = function() {
 
 }
-var StateTwo = function() {console.log("Moving on up");}
+
+
+
 var init = function() {
-//worry about states?
-//elseif: do you have dictionary, do you have a search term?
+
   StateOne();
-  //process.stdin.on('data', loadDictionary)
-
 }
-
-
 
 
 function EnterOperation(){
--- src/modules/loading.js
+++ src/modules/loading.js
@@ -2,14 +2,10 @@ const dictionary = require('../../data/dictionary.json');
 const cli = require('./cli_interface');
 var selectedDictionary;
 
-//var dictionarys = { }
-//display hashtable of dictionaries
-//^^^^ dictionaries available
-
-
-//check user input
-
-//if incorrect, send back to init()
+var displayDictionaries = function() {
+  console.log("Select a dictionary...");
+  console.log("1. Dictionary");
+}
 
 var FindDictionary = function (str){
   str = str.trim();
@@ -17,32 +13,20 @@ var FindDictionary = function (str){
     console.log("Sucessfully loaded your dictionary!");
    var selectedDictionary = dictionary;
    console.log("Word count: " + Object.keys(selectedDictionary).length);
-   console.log(letterFrequency(selectedDictionary));
-   return true
+   letterFrequency(selectedDictionary);
+   return true;
   }
   else {
-    console.log("you suck there is no dictionary named that");
-    return false
-    //cli.init();
+    console.log("Invalid Response");
+    return false;
   }
-
-//if str is a dictionary load that dictionary
-//then send it back to cli interface for search term
-//display Successfully loaded: dictionary.json
-// Word count: 12345
-// Word frequency by starting letter:
-// A: 123
-// B: 456
-
 }
 
 var letterFrequency = function(dict) {
   var letterFrequencyObj = {
-
   }
 var dictword = Object.keys(dict);
   for(i=0; i<dictword.length; i++) {
-
     var word = dictword[i];
     var firstLetter = word[0];
 
@@ -52,19 +36,19 @@ var dictword = Object.keys(dict);
     else {
         letterFrequencyObj[firstLetter] += 1;
       }
-      //console.log(word + firstLetter + letterFrequencyObj);
     }
-    //console.log(letterFrequencyObj["a"] + "where is this");
 DisplayWordCount(letterFrequencyObj);
     return letterFrequencyObj;
-  }
+}
+
 var DisplayWordCount = function(wordCount){
   for (var k in wordCount) {
-       if (wordCount.hasOwnProperty(k)) {
-          console.log(wordCount[k] + " words with " + k );
-       }
+      if (wordCount.hasOwnProperty(k)) {
+        console.log(wordCount[k] + " words with " + k );
+      }
+  }
 }
 
-}
-
-module.exports = { "FindDictionary": FindDictionary}
+module.exports =
+{ "FindDictionary": FindDictionary,
+"displayDictionaries": displayDictionaries}
