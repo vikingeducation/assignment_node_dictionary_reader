c81c2c8bc4be047f6290df31e441b1808c412c7c
Brandon <bparkerproductions@gmail.com>
2017-08-17 21:32:15 -0500

    warm up

---
+++ warmup/index.js
@@ -0,0 +1,57 @@
+var count = 1;
+
+function one(state) {
+
+  // Start listening to STDIN
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  // Inline function to handle
+  // message output
+  var showMessage = (err) => {
+    console.log(`State ${state}`);
+    console.log('Type "next" to continue');
+    if (err) {
+      console.error(err);
+    }
+  };
+
+  // Display message
+  showMessage();
+
+
+  // Handler for STDIN data
+  // event
+  var onData = (data) => {
+    data = data.trim();
+
+    // If user input "next"
+    // let's go to the next
+    // state
+    if (data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+
+      console.log(count);
+      if(count < 3){
+        count++;
+        one(count);
+      }
+      else{
+        console.log("Goodbye!");
+        process.exit();
+      }
+
+    } else {
+
+      // All other input is invalid
+      showMessage(`Invalid: ${ data }`);
+    }
+  };
+
+  // Set the listener
+  process.stdin.on('data', onData);
+}
+
+// Start the app
+one(count);
