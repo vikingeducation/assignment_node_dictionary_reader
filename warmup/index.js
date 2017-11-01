1262803b640256b8550933488721a3c5f353dac1
Jared Gebel <jaredjgebel@gmail.com>
2017-10-31 21:07:29 -0400

    Add warmup exercise

---
+++ warmup/index.js
@@ -0,0 +1,97 @@
+// Does node automatically assume that parameters are optional? RE: err in showMessage
+
+function one() {
+  // Start listening to STDIN
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  // Inline function to handle message output
+  var showMessage = (err) => {
+    console.log('State one');
+    console.log('Type "next" to continue');
+    if (err) {
+      console.error(err);
+    }
+  };
+
+  // Display message
+  showMessage();
+
+  // Handler for STDIN data event
+  var onData = (data) => {
+    data = data.trim();
+
+    // If user input "next" let's go to the next state
+    if (data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData) ;
+
+      // ------------------------------------------
+      // Go to next view here
+      // ------------------------------------------
+      two();
+
+    } else {
+      // All other input is invalid
+      showMessage(`Invalid: ${ data }`);
+    }
+  };
+
+  // Set the listener
+  process.stdin.on('data', onData);
+}
+
+function two() {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  var showSecondMessage = (err) => {
+    console.log('State two');
+    console.log('Type next to continue');
+    if (err) { console.log(err); }
+  };
+
+  showSecondMessage();
+
+  var onData = (data) => {
+    data = data.trim();
+    if(data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+
+      // Go to next listener
+      three();
+
+    } else {
+      showSecondMessage(`Invalid: ${ data }`);
+    }
+  }
+  process.stdin.on('data', onData);
+}
+
+function three() {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  var showThirdMessage = (err) => {
+    console.log('State three');
+    console.log('Type next to quit');
+    if (err) { console.log(err); }
+  }
+
+  showThirdMessage();
+
+  var onData = (data) => {
+    data = data.trim();
+    if(data === 'next') {
+      process.stdin.pause();
+      process.stdin.removeListener('data', onData);
+      console.log('Goodbye');
+    } else {
+      showThirdMessage(`Invalid: ${ data }`);
+    }
+  }
+}
+
+// Start the app
+one();
