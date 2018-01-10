977d5f74ad78b1508a90a247cd3117afd7ed3562
Ann Allan <ann.cristina.allan@gmail.com>
2018-01-10 15:35:57 -0500

    Add solutions

---
+++ warmup/index.js
@@ -0,0 +1,44 @@
+function one () {
+  // Start listening to STDIN
+  process.stdin.resume()
+  process.stdin.setEncoding('utf8')
+
+  // Inline function to handle
+  // message output
+  var showMessage = (err) => {
+    console.log('State one')
+    console.log('Type "next" to continue')
+    if (err) {
+      console.error(err)
+    }
+  }
+
+  // Display message
+  showMessage()
+
+  // Handler for STDIN data
+  // event
+  var onData = (data) => {
+    data = data.trim()
+
+    // If user input "next"
+    // let's go to the next
+    // state
+    if (data === 'next') {
+      process.stdin.pause()
+      process.stdin.removeListener('data', onData)
+
+      // ----------------------------------------
+      // Go to next view here
+      // ----------------------------------------
+    } else {
+      // All other input is invalid
+      showMessage(`Invalid: ${data}`)
+    }
+  }
+  // Set the listener
+  process.stdin.on('data', onData)
+}
+
+// Start the app
+one()
