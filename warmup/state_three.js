1f49c37368b52da871f1df93cbee692570b05822
anne <richardson.ae@gmail.com>
2018-02-06 16:52:35 -0600

    Finishes warmup state changers

--- warmup/state_three.js
+++ warmup/state_three.js
@@ -1,44 +1,29 @@
-var two = require('./state_two');
+function three() {
 
-function one() {
-
-  // Start listening to STDIN
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
 
-  // Inline function to handle message output
   var showMessage = (err) => {
-    console.log('State one');
+    console.log('State three');
     console.log('Type "next" to continue');
     if (err) {
       console.error(err);
     }
   };
 
-  // Display message
   showMessage();
 
-  // Handler for STDIN data event
   var onData = (data) => {
     data = data.trim();
 
-    // If user input "next" let's go to the next state
-    if (data === 'next') {
+    if (data === 'next' || data === 'q' || data === 'quit') {
       process.stdin.pause();
       process.stdin.removeListener('data', onData);
-
-      // Go to next view here
-      two();
-
     } else {
-
-      // All other input is invalid
       showMessage(`Invalid: ${ data }`);
     }
   };
-
-  // Set the listener
   process.stdin.on('data', onData);
 }
 
-module.exports = one;
+module.exports = three;
--- warmup/state_two.js
+++ warmup/state_two.js
@@ -1,44 +1,33 @@
-var two = require('./state_two');
+var three = require('./state_three');
 
-function one() {
+function two() {
 
-  // Start listening to STDIN
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
 
-  // Inline function to handle message output
   var showMessage = (err) => {
-    console.log('State one');
+    console.log('State two');
     console.log('Type "next" to continue');
     if (err) {
       console.error(err);
     }
   };
 
-  // Display message
   showMessage();
 
-  // Handler for STDIN data event
   var onData = (data) => {
     data = data.trim();
 
-    // If user input "next" let's go to the next state
     if (data === 'next') {
       process.stdin.pause();
       process.stdin.removeListener('data', onData);
-
-      // Go to next view here
-      two();
-
+      three();
     } else {
-
-      // All other input is invalid
       showMessage(`Invalid: ${ data }`);
     }
   };
 
-  // Set the listener
   process.stdin.on('data', onData);
 }
 
-module.exports = one;
+module.exports = two;
